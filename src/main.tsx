9fe0e62c3dd6fd9fa2202cbd3e99533d016ddf97
AndresSanRo <andres.san.ro@outlook.com>
2023-09-30 18:05:52 +0200

    add az function

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+await fetch("http://localhost:7071/api/HelloWorld")
+  .then(async (response) => console.log(await response.text()))
+  .catch(() => console.error("Sergio es maricón"));
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
