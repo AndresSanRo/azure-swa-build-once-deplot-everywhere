2312690af861f8f8010b04bf541e4795c965d2bd
AndresSanRo <andres.san.ro@outlook.com>
2023-09-30 18:49:19 +0200

    stuff

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-fetch("http://localhost:7071/api/HelloWorld")
+fetch("/api/HelloWorld")
   // fetch(`${import.meta.env.VITE_SWA_API}/HelloWorld`)
   .then(async (response) => console.log(await response.text()))
   .catch((err) => console.error(err));
