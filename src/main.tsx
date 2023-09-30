a35ef109f64984c2e8a99a09012a91062ad66ea9
AndresSanRo <andres.san.ro@outlook.com>
2023-09-30 18:07:43 +0200

    remove top level await

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-await fetch("http://localhost:7071/api/HelloWorld")
+fetch("http://localhost:7071/api/HelloWorld")
   .then(async (response) => console.log(await response.text()))
   .catch(() => console.error("Sergio es maricón"));
 
