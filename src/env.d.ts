a93a4db3c2c047e2fc985fab4695c55ba784042a
AndresSanRo <andres.san.ro@outlook.com>
2023-09-30 18:37:18 +0200

    add environments

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly VITE_SWA_API: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-fetch("http://localhost:7071/api/HelloWorld")
+fetch(`${import.meta.env.VITE_SWA_API}/HelloWorld`)
   .then(async (response) => console.log(await response.text()))
-  .catch(() => console.error("Sergio es maricón"));
+  .catch((err) => console.error(err));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
