c87576de7ef0eb0505f9676b61c190d9126bb1be
AndresSanRo <andres.san.ro@outlook.com>
2023-10-07 18:55:24 +0200

    feat: add app config

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddEnvironmentVariables();
+        var settings = config.Build();
+        var appConfConnStr = settings.GetValue<string>("AppConfigConnectionString");
+
+        config.AddAzureAppConfiguration(options =>
+        {
+            options.Connect(appConfConnStr);
+        });        
+    })
     .ConfigureFunctionsWorkerDefaults()
     .Build();
 
