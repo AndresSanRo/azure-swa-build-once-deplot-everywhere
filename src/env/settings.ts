b46a89bfa2fe69cb61b9e3bad279fdb8bd8a41f8
AndresSanRo <andres.san.ro@outlook.com>
2023-10-10 21:45:20 +0200

    feat: add set settings

--- src/env/settings.ts
+++ src/env/settings.ts
@@ -1,3 +1,9 @@
 export interface Settings {
   HelloWorld: string;
 }
+
+export let settings: Settings | null = null;
+
+export const setSettings = (settingValues: Settings) => {
+  settings = settingValues;
+};
